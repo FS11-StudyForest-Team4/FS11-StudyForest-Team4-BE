f9a5980c1dd9e7e65425026c0941e4c175db385f
김성민 <neraziim@gmail.com>
2026-02-03 11:31:56 +0900

    feat: apply authentication to routes and update validation schemas
    - implement examples of study creation and password verify endponts with JWT
    - add Zod schemas for request body validation
    - chore: add varsIgnorePattern: '^_'

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { studyRouter } from './studies/index.js'
+import { studyRouter } from './studies/index.js';
 import { habitsRouter } from './habits/habits.routes.js';
 import { emojiRouter } from './emojis/index.js';
+import { authRouter } from './auth/index.js';
 import { HTTP_STATUS } from '#constants';
 
 export const router = express.Router();
@@ -12,8 +13,11 @@ router.get('/', (req, res) => {
     .send({ now: new Date().toISOString(), Message: 'OK' });
 });
 
-router.use('/studies', studyRouter)
-router.use('/habits', habitsRouter)
+router.use('/studies', studyRouter);
+router.use('/habits', habitsRouter);
 
 // /emojis 경로에 Emoji 라우터 연결
 router.use('/emojis', emojiRouter);
+
+// /auth 경로(모달) auth router 연결
+router.use('/auth', authRouter);
