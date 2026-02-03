ca5844ae732bae593d7c85669e954698667402b5
sumforest-ha <harim227@gmail.com>
2026-02-03 14:48:22 +0900

    fix:라우터 우선순위 변경 cherry-pick

--- src/routes/studies/studies.schema.js
+++ src/routes/studies/studies.schema.js
@@ -38,7 +38,9 @@ export const findStudySchema = z.object({
 
 //스터디 생성 스키마
 export const createStudySchema = z.object({
-  password: z.string().min(4, '비밀번호는 4자리 이상이어야 합니다.'), //비밀번호 4자리 이상 조건 임의로 달았슴다.
+  password: z
+    .string({ required_error: '비밀번호는 필수입니다.' })
+    .min(4, '비밀번호는 4자리 이상이어야 합니다.'), //비밀번호 4자리 이상 조건 임의로 달았슴다.
   title: z.string().min(1, '스터디 제목은 필수입니다.'),
   description: z.string(),
   nickName: z.string().min(1, '닉네임은 필수입니다.'), //사용자 입장에서는 시각적으로 구별할 방법이 닉넴+제목밖에 없을듯 해서 추가
